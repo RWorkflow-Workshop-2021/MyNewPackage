ecd08c35961d61a80597302523f27ea4959a38eb
Eli Holmes <eli.holmes@noaa.gov>
2021-07-09 11:11:09 -0700

    second update

---
+++ R/data-WWW2.R
@@ -0,0 +1,12 @@
+#' @title WWW2
+#'
+#' @description WWW data squared.
+#'
+#' \itemize{
+#'   \item price. price in US dollars
+#'   \item carat. weight of the diamond
+#' }
+#'
+#' @usage data(WWW2)
+#' @format A ts object.
+"WWW2"
