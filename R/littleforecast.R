77b693435c6a18466e41b269b67b64078bc9213e
Eli Holmes <eli.holmes@noaa.gov>
2021-07-23 12:35:30 -0700

    add some workflows

--- R/littleforecast.R
+++ R/littleforecast.R
@@ -1,8 +1,19 @@
 #' Little Forecast
 #'
 #' This does an arima forecast
-littleforecast <- function(data, nyears=10){
+#'
+#' @examples
+#' library(stats)
+#' lm(a ~ b, data=df)
+littleforecast <- function(data, nyears=10, type=c("red", "blue", "green")){
+  # tests
+  if(!is.integer(nyears)) stop("nyears must be integer")
+  type <- match.arg(type)
+
   fit <- forecast::auto.arima(data)
+  for(i in 1:10000){
+    cat(i)
+  }
   fc <- forecast::forecast(fit, h = nyears)
   ggplot2::autoplot(fc)
 }
